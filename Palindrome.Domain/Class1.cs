c168ca93df6f236b4ff8a71565f308479e127765
james griggs <griggsjames0806@gmail.com>
2020-07-20 17:50:31 -0400

    Palindrome

--- Palindrome.Domain/Class1.cs
+++ Palindrome.Domain/Class1.cs
@@ -2,7 +2,29 @@
 
 namespace Palindrome.Domain
 {
-    public class Class1
+    class Program
     {
+        static void Main()
+        {
+            Palindrome();
+        }
+
+        static void Palindrome()
+        {
+            var input = (Console.ReadLine());
+            int leftside = 0;
+            int rightside = (input.Length -1);
+
+            while(rightside > 1)
+            {
+                if(input[leftside++] != input[rightside--])
+                {
+                    Console.WriteLine(" Input given is not a palindrome");
+                    return;
+                }
+            }
+
+            Console.WriteLine(" Input is a Palindrome");
+        }
     }
 }
